c4ddbd7b710de6ee1e27b75f3f8cc065b16d4c52
Alex <143653122+alca1296@users.noreply.github.com>
2026-04-25 23:21:27 -0600

    did human player

--- MahjongTile/MahjongTile.cs
+++ MahjongTile/MahjongTile.cs
@@ -6,36 +6,59 @@ namespace Mahjong;
 [Tool]
 public partial class MahjongTile : PanelContainer
 {
-    private MahjongTileRecord _tile = null;
+	private MahjongTileRecord _tile = null;
 
-    public MahjongTileRecord Tile
-    {
-        get => _tile;
-        set
-        {
-            _tile = value;
-            UpdateUI();
-        }
-    }
+	public MahjongTileRecord Tile
+	{
+		get => _tile;
+		set
+		{
+			_tile = value;
+			UpdateUI();
+		}
+	}
 
-    public override void _Ready()
-    {
-        UpdateUI();
-    }
+	public override void _Ready()
+	{
+		UpdateUI();
+	}
 
-    private void UpdateUI()
-    {
-        var label = GetNodeOrNull<Label>("MarginContainer/Label");
+	private void UpdateUI()
+	{
+		var label = GetNodeOrNull<Label>("MarginContainer/Label");
 
-        if (label == null) return;
+		if (label == null) return;
 
-        if (_tile == null)
-        {
-            label.Text = "No tile value set.";
-        }
-        else
-        {
-            label.Text = _tile.ToString(); ;
-        }
-    }
+		if (_tile == null)
+		{
+			label.Text = "No tile value set.";
+		}
+		else
+		{
+			label.Text = _tile.ToString(); ;
+		}
+	}
+
+	public override void _GuiInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+			{
+				HandleTileClicked();
+				AcceptEvent();
+			}
+		}
+	}
+
+	private void HandleTileClicked()
+	{
+		var gameScene = GetTree().CurrentScene as GameManager;
+
+		if (gameScene != null)
+		{
+			gameScene.NotifyTileClicked(_tile);
+			GD.Print($"Clicked: {_tile}");
+		}
+	}
 }
--- Player/Player.cs
+++ Player/Player.cs
@@ -1,36 +1,49 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Mahjong;
 
 public class Player
 {
-    // The "Brain" of the player (can be AI or Human)
-    private readonly IPlayerDecisionMaker _decisionMaker;
+	// The "Brain" of the player (can be AI or Human)
+	private readonly IPlayerDecisionMaker _decisionMaker;
 
-    // The state of the player
-    private readonly TileHandData _hand = new();
-    public TileHandData Hand => _hand;
+	// The state of the player
+	private readonly TileHandData _hand = new();
+	public TileHandData Hand => _hand;
 
-    public Player(IPlayerDecisionMaker decisionMaker)
-    {
-        _decisionMaker = decisionMaker;
-    }
+	public Player(IPlayerDecisionMaker decisionMaker)
+	{
+		_decisionMaker = decisionMaker;
+	}
 
-    public void ReceiveTile(MahjongTileRecord tile) => _hand.AddConcealed(tile);
-    public bool Discard(MahjongTileRecord tile) => _hand.Discard(tile);
+	public void ReceiveTile(MahjongTileRecord tile) => _hand.AddConcealed(tile);
+	public bool Discard(MahjongTileRecord tile) => _hand.Discard(tile);
 
-    public StealDecision DecideStealOrPass(MahjongTileRecord lastDiscard, bool isNextPlayer) => _decisionMaker.DecideStealOrPass(Hand, lastDiscard, isNextPlayer);
+	public async Task<StealDecision> DecideStealOrPass(MahjongTileRecord lastDiscard, bool isNextPlayer) => await _decisionMaker.DecideStealOrPass(Hand, lastDiscard, isNextPlayer);
 
-    public MahjongTileRecord DecideDiscard() => _decisionMaker.DecideDiscard(Hand);
+	public Task<MahjongTileRecord> DecideDiscard() => _decisionMaker.DecideDiscard(Hand);
+
+	public void notifyTileClicked(MahjongTileRecord tile)
+	{
+		_decisionMaker.NotifyTileClicked(tile);
+	}
 }
 
 public class PlayerFactory
 {
-    public static Player createGreedyBot() {
-        return new Player(DecisionMakerFactory.newGreedyBotStrategy());
-    }
+	public static Player createGreedyBot()
+	{
+		return new Player(DecisionMakerFactory.newGreedyBotStrategy());
+	}
 
-    public static Player createDumbBot() {
-        return new Player(DecisionMakerFactory.newDumbBotStrategy());
-    }
+	public static Player createDumbBot()
+	{
+		return new Player(DecisionMakerFactory.newDumbBotStrategy());
+	}
+
+	public static Player createHumanPlayer()
+	{
+		return new Player(DecisionMakerFactory.newHumanPlayerStrategy());
+	}
 }
