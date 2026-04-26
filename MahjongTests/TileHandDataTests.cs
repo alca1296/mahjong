f3ff70acb9149505d0bd6169fd8bf28b509570aa
Alex <143653122+alca1296@users.noreply.github.com>
2026-04-25 18:27:26 -0600

    started on ui

--- MahjongTests/TileHandDataTests.cs
+++ MahjongTests/TileHandDataTests.cs
@@ -4,50 +4,50 @@ namespace MahjongTests;
 
 public class TileHandDataTests
 {   
-    private static MahjongTileRecord ArbitraryTile => new Dragon(DragonColor.Red);
-    
-    [Fact]
-    public void AddConcealed()
-    {
-        var hand = new TileHandData();
-        var tile = ArbitraryTile;
-        
-        hand.AddConcealed(tile);
-        Assert.Contains(tile, hand.ConcealedTiles);
-    }
-    
-    [Fact]
-    public void AddMeld()
-    {
-        var hand = new TileHandData();
-        var meld = new Meld(MeldType.Triplet, new[] {ArbitraryTile, ArbitraryTile, ArbitraryTile});
-        
-        hand.AddMeld(meld);
-        Assert.Contains(meld, hand.Melds);
-    }
+	private static MahjongTileRecord ArbitraryTile => new Dragon(DragonColor.Red);
+	
+	[Fact]
+	public void AddConcealed()
+	{
+		var hand = new TileHandData();
+		var tile = ArbitraryTile;
+		
+		hand.AddConcealed(tile);
+		Assert.Contains(tile, hand.ConcealedTiles);
+	}
+	
+	[Fact]
+	public void AddMeld()
+	{
+		var hand = new TileHandData();
+		var meld = new Meld(MeldType.Triplet, new[] {ArbitraryTile, ArbitraryTile, ArbitraryTile});
+		
+		hand.AddMeld(meld);
+		Assert.Contains(meld, hand.Melds);
+	}
 }
 
 public class DiscardPileTests
 {
-    private static MahjongTileRecord ArbitraryTile => new Dragon(DragonColor.Red);
-    
-    [Fact]
-    public void EndReturnsNullForEmptyPile()
-    {
-        var pile = new DiscardPile();
-        Assert.Null(pile.End);
-        Assert.Null(pile.TakeEnd());
-    }
-    
-    [Fact]
-    public void EndReturnsLastAddedTile()
-    {
-        var pile = new DiscardPile();
-        pile.Add(new Dragon(DragonColor.White));
-        pile.Add(ArbitraryTile);
-        Assert.Equal(ArbitraryTile, pile.End);
-        var tile = pile.TakeEnd();
-        Assert.Equal(ArbitraryTile, tile);
-        Assert.False(pile.Contains(tile));
-    }
+	private static MahjongTileRecord ArbitraryTile => new Dragon(DragonColor.Red);
+	
+	[Fact]
+	public void EndReturnsNullForEmptyPile()
+	{
+		var pile = new DiscardPile();
+		Assert.Null(pile.End);
+		Assert.Null(pile.TakeEnd());
+	}
+	
+	[Fact]
+	public void EndReturnsLastAddedTile()
+	{
+		var pile = new DiscardPile();
+		pile.Add(new Dragon(DragonColor.White));
+		pile.Add(ArbitraryTile);
+		Assert.Equal(ArbitraryTile, pile.End);
+		var tile = pile.TakeEnd();
+		Assert.Equal(ArbitraryTile, tile);
+		Assert.False(pile.Contains(tile));
+	}
 }
